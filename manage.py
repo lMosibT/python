c3eeb3ed7c25667b8357d98e05ccc8e9c537fa49
lMosibT <fbb907853583516@163.com>
2021-05-22 18:39:07 +0800

    Session的保存位置指定&测试

--- manage.py
+++ manage.py
@@ -1,38 +1,56 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 from redis import StrictRedis
-from flask_wtf.csrf import CSRFProtect
+from flask import Flask, session
+from flask_sqlalchemy import SQLAlchemy
+# 可以用来指定 session 保存的位置
+from flask_session import Session
+
 
 class Config(object):
-	#项目的配置
-	DEBUG =  True
+    """项目的配置"""
+    DEBUG = True
 
-	#s数据库的配置信息
-	SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:mysql@127.0.0.1:3306/information"
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "iECgbYWReMNxkRprrzMo5KAQYnb2UeZ3bwvReTSt+VSESW0OB8zbglT+6rEcDW9X"
 
-	# redis配置
-	REDIS_HOST = "127.0.0.1"
-	REDIS_PORT = 6379
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information27"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Redis的配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # Session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定 Session 保存的 redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
 
 app = Flask(__name__)
-
-#加载配置
+# 加载配置
 app.config.from_object(Config)
 
-#初始化数据库
+# 初始化数据库
 db = SQLAlchemy(app)
-
-#初始化 redis 存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
-#开启当前项目 CSRF 保护，只做服务器验证功能
+# 初始化 redis 存储对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 开启当前项目 CSRF 保护，只做服务器验证功能
 CSRFProtect(app)
+# 设置session保存指定位置
+Session(app)
 
 
 @app.route('/')
 def index():
-	return 'index'
+    session["name"] = "itheima"
+    return 'index33333'
 
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run()
